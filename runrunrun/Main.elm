91ceebf5ef161831c88a84dbc0c1af8014caced9
Karol Marcjan <karol.marcjan@gmail.com>
2016-06-10 04:28:50 +0200

    Add canvas resizing and zoom

--- runrunrun/Main.elm
+++ runrunrun/Main.elm
@@ -1,38 +1,60 @@
 module Main exposing (main)
 
 import Math.Vector3 as Vec3 exposing (Vec3)
-import Html as H exposing (Html)
+import Html as H exposing (Html, Attribute)
 import Html.App as App
 import Html.Attributes as HA
+import Html.Events as HE
+import Json.Decode as Json exposing ((:=))
+import Task
+import Time exposing (Time)
 import WebGL
+import Window
 import AnimationFrame
 
 
 main : Program Never
 main =
     App.program
-        { init = () ! []
-        , subscriptions = \_ -> AnimationFrame.diffs identity
-        , update = \_ _ -> () ! []
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
         , view = view
         }
 
 
-view : a -> Html msg
-view model =
-    WebGL.toHtml
-        [ HA.width 400
-        , HA.height 300
-        ]
-        [ WebGL.render vertexShader fragmentShader heroVertices {} ]
+
+-- MODEL
+
+
+type alias Model =
+    { size : Window.Size
+    , camera : Camera
+    , hero : WebGL.Drawable Vertex
+    }
 
 
 type alias Vertex =
     { pos : Vec3 }
 
 
-heroVertices : WebGL.Drawable Vertex
-heroVertices =
+type alias Camera =
+    { zoom : Float }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { size = Window.Size 0 0
+      , camera = { zoom = 1 }
+      , hero = hero
+      }
+    , Window.size
+        |> Task.perform (\_ -> Debug.crash "never") Resize
+    )
+
+
+hero : WebGL.Drawable Vertex
+hero =
     WebGL.Triangle
         [ ( { pos = Vec3.vec3 0 0 0 }
           , { pos = Vec3.vec3 1 1 0 }
@@ -41,19 +63,86 @@ heroVertices =
         ]
 
 
-vertexShader : WebGL.Shader Vertex {} {}
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ AnimationFrame.diffs Animate
+        , Window.resizes Resize
+        ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Resize Window.Size
+    | Animate Time
+    | Zoom Float
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Resize newSize ->
+            { model | size = newSize } ! []
+
+        Zoom delta ->
+            let
+                { camera } =
+                    model
+
+                newCamera =
+                    { camera
+                        | zoom =
+                            camera.zoom
+                                + (Debug.log "zoom delta" delta / 100)
+                                |> clamp 0.1 1
+                    }
+
+                newModel =
+                    { model | camera = newCamera }
+            in
+                newModel ! []
+
+        Animate dt ->
+            model ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view { size, hero, camera } =
+    WebGL.toHtml
+        [ HA.width size.width
+        , HA.height size.height
+        , HA.style [ (,) "display" "block" ]
+        , onWheel Zoom
+        ]
+        [ WebGL.render vertexShader fragmentShader hero camera ]
+
+
+vertexShader : WebGL.Shader Vertex { u | zoom : Float } {}
 vertexShader =
     [glsl|
         precision mediump float;
 
         attribute vec3 pos;
 
+        uniform float zoom;
+
         void main() {
-            gl_Position = vec4(0.5 * pos, 1);
+            gl_Position = vec4(zoom * pos, 1);
         } |]
 
 
-fragmentShader : WebGL.Shader {} {} {}
+fragmentShader : WebGL.Shader {} u {}
 fragmentShader =
     [glsl|
         precision mediump float;
@@ -61,3 +150,13 @@ fragmentShader =
         void main() {
             gl_FragColor = vec4(0, 0, 1, 1);
         } |]
+
+
+
+-- EVENTS
+
+
+onWheel : (Float -> msg) -> Attribute msg
+onWheel f =
+    HE.on "wheel"
+        ("deltaY" := Json.float |> Json.map f)
